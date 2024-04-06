d4f6cc5e3ef367c1008edcff15e24ef90bd2c13e
mohibkay <mohibk.in@gmail.com>
2024-04-06 14:29:08 +0530

    Add GraphQL dependencies and setup GraphQL endpoint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
 import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+import { buildSchema } from "graphql";
+import { ruruHTML } from "ruru/server";
+
 import cors from "cors";
 import { corsOptions } from "./config.js";
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello() {
+    return "Hello, world!";
+  },
+};
 
 const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
+
+app.all("/graphql", createHandler({ schema, rootValue: root }));
+
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
+});
 
 import userRouter from "./routes/user.routes.js";
 import articleRouter from "./routes/article.routes.js";
